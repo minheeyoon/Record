a6373bfba78fc9fb04e7aa871424cd2b4ebc9573
minhee <mini940511@gmail.com>
2023-01-16 04:36:48 +0900

    style-dictionary 사용해서 토큰 css로 변환해 사용하기

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import Home from "./pages/home";
+import Header from "./copmonents/header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <Header />
+    <Home />
   </BrowserRouter>
 );
---
+++ blog/src/pages/home.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Test</h1>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum. 대법원에 대법관을 둔다. 다만, 법률이 정하는 바에 의하여
+        대법관이 아닌 법관을 둘 수 있다. 모든 국민은 신속한 재판을 받을 권리를
+        가진다. 형사피고인은 상당한 이유가 없는 한 지체없이 공개재판을 받을
+        권리를 가진다. 국가는 균형있는 국민경제의 성장 및 안정과 적정한 소득의
+        분배를 유지하고, 시장의 지배와 경제력의 남용을 방지하며, 경제주체간의
+        조화를 통한 경제의 민주화를 위하여 경제에 관한 규제와 조정을 할 수 있다.
+        연소자의 근로는 특별한 보호를 받는다. 대통령은 전시·사변 또는 이에
+        준하는 국가비상사태에 있어서 병력으로써 군사상의 필요에 응하거나 공공의
+        안녕질서를 유지할 필요가 있을 때에는 법률이 정하는 바에 의하여 계엄을
+        선포할 수 있다. 모든 국민은 학문과 예술의 자유를 가진다. 학교교육 및
+        평생교육을 포함한 교육제도와 그 운영, 교육재정 및 교원의 지위에 관한
+        기본적인 사항은 법률로 정한다.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
