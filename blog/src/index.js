79c1c2a724a81e096bef0d5694f68ea3d56183d6
minhee <mini940511@gmail.com>
2023-02-12 03:30:09 +0900

    build : add github pages build scripts

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Project from "./pages/project";
 import Post from "./pages/post";
